94fc3a67e5451c853f23788fc444158ff62f112d
nizaarr302 <67221359+nizaarr302@users.noreply.github.com>
2023-10-17 18:09:10 +0700

    Add files via upload

---
+++ list.php
@@ -0,0 +1,8 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$connection = new mysqli("localhost","root","","latihan");
+$data = mysqli_query($connection, "select * from produk");
+$data = mysqli_fetch_all($data, MYSQLI_ASSOC);
+
+echo json_encode($data);
+?>
